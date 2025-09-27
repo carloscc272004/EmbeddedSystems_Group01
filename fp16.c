52c74846358feebc9cb1f956dcdd05e5e96ba691
timetowinm <michael.tejada@utsa.edu>
2025-09-27 10:10:25 -0500

    Add files via upload

---
+++ fp16.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+
+//typedef
+typedef uint16_t fp16_t;
+//DEFINES HERE 
+
+ 
+#define FP32_BIAS 127
+#define FP16_BIAS 15
+
+//23 BITS AND 8 BITS
+#define FP32_MANT_BITMASK 0x7FFFFF
+#define FP32_EXP_BITMASK 0XFF
+
+#define FP32_SIZE_OF_MANT 23
+#define FP32_EXP_OFFSET FP32_SIZE_OF_MANT
+#define FP32_SIGN_OFFSET 31
+
+//10 BITS AND 5 BITS
+#define FP16_MANT_BITMASK 0x3FF
+#define FP16_EXP_BITMASK 0X1F
+
+#define FP16_SIZE_OF_MANT 10
+#define FP16_EXP_OFFSET FP16_SIZE_OF_MANT
+#define FP16_SIGN_OFFSET 15
+
+
+
+#define SINGLE_BIT_MASK 0x01
+
+
+//GLLOBAL FUNCTION PROTOTYPES-> COMMENTS AT FUNCTION DECLARATION
+
+fp16_t FP16(const float * const restrict x);
+float float_from_fp16(const fp16_t  * const restrict x);
+ void print_fp16(const fp16_t * const restrict x);
+ //STATIC HELPER FUNCTION  PROTOTYPES -> COMMENTS AT FUNCTION DECLARATION
+
+static inline void fp16_decompose(const fp16_t  * const restrict x, int * const restrict sign, int * const restrict exponent, uint16_t * const restrict mant);
+static inline double fp16_cal_msum(const uint16_t mantissa);
+static inline float fp16_parts_tofloat(int sign, int exponent, float mant_sum );
+
+static inline void fp32_decompose(const uint32_t * const restrict x,uint8_t * const restrict sign_bit, uint8_t * const restrict exp_byte, uint32_t * const restrict mant);
+static inline fp16_t fp32_parts_tofp16(const uint8_t * const restrict sign_bit, uint8_t * const restrict exp_byte, uint32_t * const restrict mant);
+
+
+/************
+ * MAIN
+ */
+int main(void){
+    float x = -5.7525; 
+     printf("float is %f\r\n",x);
+    fp16_t half_x = FP16(&x);
+   
+     print_fp16(&half_x);
+   
+}
+
+
+
+
+
+/***************8
+ * CONVERT FLOAT TO FP16
+ */
+fp16_t FP16(const float * const restrict x){
+
+  
+    uint32_t bare_x = 0;
+    //we want the raw unprotected bytes of the float.  memcpy works for this
+    memcpy(&bare_x,x,sizeof(bare_x));
+
+        uint8_t sign = 0;
+    uint32_t mantissa = 0;
+    uint8_t exponent = 0;
+
+    fp32_decompose(&bare_x,&sign,&exponent,&mantissa);
+  
+        
+   
+    fp16_t half_prec_float = fp32_parts_tofp16(&sign, &exponent, &mantissa);
+
+    //optional debug messages
+    // printf("float bytes are %x and bare bytes are  %x\r\n",*x,bare_x);
+    // printf("sign bit is %x, mantissa is %x, exponent is %x\r\n ", sign,mantissa,exponent);
+    //  printf("half_prec_float bytes are %x",half_prec_float);
+
+    return half_prec_float;
+
+}
+
+
+/*******************8
+ * CONVERT FP16 TO FLOAT
+ */
+float float_from_fp16(const fp16_t  * const restrict x){
+
+int sign = 0;
+int exponent = 0;
+uint16_t mantissa = 0;
+
+fp16_decompose(x,&sign,&exponent,&mantissa);
+
+float mant_sum = fp16_cal_msum(mantissa);
+
+// optional debug statements
+//  printf("p:exponent = %x ",exponent);
+// printf("p:mantissa = %x ",mantissa);
+// printf("p:mant_sum = %f \r\n",mant_sum);
+
+
+return fp16_parts_tofloat(sign,exponent,mant_sum);
+
+
+}
+
+
+/***************
+ * PRINT AN FP16 VALUE
+ */
+ void print_fp16(const fp16_t * const restrict x){
+
+float converted = float_from_fp16(x);
+ 
+//print value
+printf("%f\r\n",converted);
+return;
+//debug message
+//  printf("\r\n END OF PRINT\r\n");
+}
+
+
+/**************
+ * INLINE HELPER FUNCTIONS
+ */
+
+
+ /*****
+  * DECOMPOSE FP32 TO ITS PARTS
+  */
+static inline void fp32_decompose(const uint32_t * const restrict x,uint8_t * const restrict sign_bit, uint8_t * const restrict exp_byte, uint32_t * const restrict mant){
+    *sign_bit = ((*x >> FP32_SIGN_OFFSET) &  0x01);
+    *exp_byte =  ((*x >> FP32_EXP_OFFSET) & 0xFF) - FP32_BIAS;
+    *mant = (*x) & FP32_MANT_BITMASK;
+
+    return;
+}
+
+/********
+ * COMBINE FP32 PARTS INTO FP16
+ * 
+ */
+static inline fp16_t fp32_parts_tofp16(const uint8_t * const restrict sign_bit, uint8_t * const restrict exp_byte, uint32_t * const restrict mant){
+     //add 16 bit bias and truncate to 5 bits
+    *exp_byte = (*exp_byte + FP16_BIAS) & FP16_EXP_BITMASK;
+    //truncate to 10 bits
+    *mant = (((*mant >> (FP32_SIZE_OF_MANT - FP16_SIZE_OF_MANT)) & FP16_MANT_BITMASK));
+
+    
+    return ((*sign_bit ) << FP16_SIGN_OFFSET) | ((*exp_byte) << FP16_EXP_OFFSET) | (*mant);
+ 
+   
+}
+
+
+/***********8
+ * DECOMPOSE FP16 TO ITS PARTS
+ */
+static inline void fp16_decompose(const fp16_t  * const restrict x, int * const restrict sign, int * const restrict exponent, uint16_t * const restrict mant){
+    //return only single sign bit
+    *sign = ((*x >> FP16_SIGN_OFFSET) == 1)?-1:1;
+    *exponent = ((*x >> FP16_EXP_OFFSET) & FP16_EXP_BITMASK) - FP16_BIAS;
+    *mant = (*x & FP16_MANT_BITMASK);
+    return;
+}
+
+
+/**********8
+ * CALCULATE THE VALUE OF FP16_MANTISSA
+ */
+static inline double fp16_cal_msum(const uint16_t mantissa){
+double mantissa_sum = 0;
+//calculate mantissa by summing each decimal bit position multiplied by its corresponding power of two
+for(uint8_t i = 1;i <= FP16_SIZE_OF_MANT;i++){
+
+    mantissa_sum += (((mantissa >> (FP16_SIZE_OF_MANT - i)) & SINGLE_BIT_MASK) * pow(2.0,(double)( -i )));
+
+} 
+return mantissa_sum;
+}
+
+
+/*************
+ * TURN FP16 PARTS INTO A FLOAT
+ */
+static inline float fp16_parts_tofloat(const int sign, const int exponent, const float mant_sum ){
+    //float should be 1.mantissa * sign * 2 to power exponent
+    return sign * (1.0 + mant_sum) * pow(2,(double)exponent);
+
+}
